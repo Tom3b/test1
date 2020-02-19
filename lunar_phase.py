b543d1f6b24e4dba4cd59fc448c4cd42f8e100dc
Tom3b <61159285+Tom3b@users.noreply.github.com>
2020-02-19 11:54:30 +0000

    added day info to lunar phase

--- lunar_phase.py
+++ lunar_phase.py
@@ -5,51 +5,66 @@ from datetime import datetime
 from datetime import timedelta
 from decimal import Decimal
 
-# calculating days since known new moon date
-d1 = datetime.now()
-d2 = datetime(2020, 1, 24, 21, 42)
-delta = d1 - d2
-days = delta.days
+#determining lunar phase
+def lunar_phase ():
+    # calculating days since known new moon date
+    d1 = datetime.now()
+    d2 = datetime(2020, 1, 24, 21, 42)
+    delta = d1 - d2
+    days = delta.days
+    seconds = delta.seconds / 86400     # 86400 seconds in a day
+    timesn = days + seconds             # time since new moon
+    p1 = timesn / 29.53                 # new moon repeats every 29.53 days
+    global phase
+    phase = Decimal(p1)                 # percentage of current cycle 0% & 100% being new 50% being full
+    daynum = 29.53 * float(phase)
 
-#determining lunor phase 
-seconds = delta.seconds / 86400     # 86400 seconds in a day
-timesn = days + seconds             # time since new moon
-p1 = timesn / 29.53                 # new moon repeats every 29.53 days
-phase = Decimal(p1)                 # percentage of current cycle 0% & 100% being new 50% being full
+    #determining lunar phase name 
+    if 0 <= phase < 0.11:
+        print ("Lunar Phase: New, day {:.1f}" .format(daynum))
+        return phase
+    elif 0.11 <= phase < 0.22:
+        print("Lunar Phase: Waxing Cresent, day {:.1f}" .format(daynum))
+        return phase
+    elif 0.22 <= phase < 0.33:
+        print("Lunar Phase: First Quarter, day {:.1f}" .format(daynum))
+        return phase
+    elif 0.33 <= phase < 0.44:
+        print("Lunar Phase: Waxing Gibbous, day {:.1f}" .format(daynum))
+        return phase
+    elif 0.44 <= phase < 0.55:
+        print("Lunar Phase: Full, day {:.1f}" .format(daynum))
+        return phase
+    elif 0.55 <= phase < 0.66:
+        print("Lunar Phase: Waning Gibbous, day {:.1f}" .format(daynum))
+        return phase
+    elif 0.77 <= phase < 0.88:
+        print("Lunar Phase: Third Quarter, day {:.1f}" .format(daynum))
+        return phase
+    elif 0.88 <= phase < 1:
+        print("Lunar Phase: Waning Crescent, day {:.1f}" .format(daynum))
+        return phase
+    else:
+        print("error")
+        pass
 
-#determining lunar phase name 
-if 0 <= phase < 0.11:
-    print ("Lunar phase: New")
-elif 0.11 <= phase < 0.22:
-    print("Lunar phase: Waxing Cresent")
-elif 0.22 <= phase < 0.33:
-    print("Lunar phase: First Quarter")
-elif 0.33 <= phase < 0.44:
-    print("Lunar phase: Waxing Gibbous")
-elif 0.44 <= phase < 0.55:
-    print("Lunar phase: Full") 
-elif 0.55 <= phase < 0.66:
-    print("Lunar phase: Waning Gibbous")
-elif 0.77 <= phase < 0.88:
-    print("Lunar phase: Third Quarter")
-elif 0.88 <= phase < 1:
-    print("Lunar phase: Waning Crescent")
-else:
-    print("error")
+#determining moon size as percentage
+def moon_size (phase):
+    if phase <= 0.5:
+        size = 200*phase
+        print("Moon Size: {0:.2f}%".format(size))
+        return size
+    elif 0.5 < phase < 1:
+        size = 200*(1-phase)
+        print("Moon Size: {0:.2f}%".format(size))
+        return size
+    else:
+        print("error")
+        pass
 
-#determining moon size as percentage 
-if phase <= 0.5:
-    size = 200*phase
-    print("Moon size: {0:.2f}%".format(size))  
-elif 0.5 < phase < 1:
-    size = 200*(1-phase)
-    print("Moon size: {0:.2f}%".format(size))
-else:
-    print("error")
+# main path of program
+lunar_phase();
+moon_size(phase);
 
-if 0 <= phase < 0.11:
-    print ("Lunar phase: New")
-elif 0.11 <= phase < 0.22:
-    print("Lunar phase: Waxing Cresent")
-elif 0.22 <= phase < 0.33:
-    print("Lunar phase: First Quarter") 
+
+
